433643b65610ee3028ddfe941e8c639bcac18358
makiprogrammer <63451795+makiprogrammer@users.noreply.github.com>
2022-08-22 16:59:37 +0200

    refactor: :recycle: Refactor CLI

--- src/index.ts
+++ src/index.ts
@@ -5,30 +5,26 @@ import { Command } from "commander";
 
 import analyseDirectories from "./analyseDirectories";
 import syncDirectories from "./syncDirectories";
+import { logWarning } from "./utils";
 
 const program = new Command();
 program.name("dirsync").description("CLI to syncing directories");
 
 program
-	.command("analyse")
-	.alias("a")
-	.alias("analyze")
-	.description(
-		"Analyse two directories. The results will be printed to the console or specified output file."
+	.description("Synchronises two directories. We recommend running `dirsync --analyse` first.")
+	.argument("<dir1>", "first directory")
+	.argument("<dir2>", "second directory")
+	.option(
+		"-a, --analyse, --analyze, --no-sync",
+		"Analyse two directories with no sync. The results will be printed to the console or specified output file specified in option --output-file."
 	)
-	.argument("<dir1>", "first directory")
-	.argument("<dir2>", "second directory")
 	.option("-o, --output-file <file>", "where to write the output of the analysis in JSON format")
-	.action(analyseDirectories);
-
-program
-	.command("synchronise")
-	.alias("sync")
-	.alias("synchronize")
-	.description("Synchronises two directories. We recommend running `dirsync analyse` first.")
-	.argument("<dir1>", "first directory")
-	.argument("<dir2>", "second directory")
 	.option("-f, --force", "asks no questions and syncs everything (not recommended)")
-	.action(syncDirectories);
+	.action((dir1, dir2, options) => {
+		if (options.analyse) return analyseDirectories(dir1, dir2, options);
+		if (options.outputFile)
+			logWarning("Specified output file has no effect when --analyse is not specified");
+		syncDirectories(dir1, dir2, options);
+	});
 
 program.parse();
